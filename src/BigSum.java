444495cf8dc91fca521ef80df1d0a1bd21a95fd6
coderbgirl <37058497+coderbgirl@users.noreply.github.com>
2018-06-12 14:39:28 -0400

    warmup

---
+++ src/BigSum.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class BigSum {
+	public static long arraySum(int a, long arr[]) {
+		long sum = 0;
+		for (int i = 0; i < a; i++) {
+			sum = sum + arr[i];
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter no.:");
+		int n = scan.nextInt();
+		long[] array = new long[n];
+		System.out.println("Enter " + n + " elements:");
+		for (int i = 0; i < n; i++) {
+			array[i] = scan.nextInt();
+		}
+
+		long result = arraySum(n, array);
+		System.out.println("Answer is:" + result);
+
+	}
+
+}
