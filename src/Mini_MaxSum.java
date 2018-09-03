7ea401e7b262105628cc88abe837c7f26b27ef59
coderbgirl <37058497+coderbgirl@users.noreply.github.com>
2018-09-03 16:06:40 -0400

    implementation

---
+++ src/Mini_MaxSum.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Mini_MaxSum {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		long[] array = new long[5];
+		System.out.println("Enter 5 no.:");
+
+		for (int i = 0; i < 5; i++) {
+			array[i] = scan.nextLong();
+		}
+		Arrays.sort(array);
+		for (int i = 0; i < 5; i++) {
+			System.out.println(array[i]);
+		}
+		long min=0;
+		for(int i=0; i<4; i++){
+			min= (min+ array[i]);
+		}
+		System.out.println(min);
+		
+		long max=0;
+		for(int i=1; i<5; i++){
+			max = (max+ array[i]);
+		}
+		System.out.println(max);
+	}
+}
