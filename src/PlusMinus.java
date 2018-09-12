b21b7d2d0988440d30c523f20dd85db77afab23c
coderbgirl <37058497+coderbgirl@users.noreply.github.com>
2018-09-12 14:00:10 -0400

    implementation

---
+++ src/PlusMinus.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class PlusMinus {
+
+	public static void main(String[] args ) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter no.:");
+		int n = scan.nextInt();
+		int[] array = new int[n];
+		System.out.println("Enter " + n + " elements:");
+		int Positive=0;
+		int Negative=0;
+		int Zero =0;
+		for (int i = 0; i < n; i++) {
+			array[i] = scan.nextInt();
+			
+
+			if(array[i]==0){
+				Zero ++;
+			}
+			else if(array[i]!=Math.abs(array[i])){
+				Negative ++;
+			}
+			else if(array[i]==Math.abs(array[i])){
+				Positive ++;
+			}
+		}
+	
+		double po= (double)Positive / n;
+		double ne=(double)Negative/n;
+		double ze=(double)Zero/n;
+		System.out.printf("%f \n", po);
+		System.out.printf("%.8f \n",ne);
+		System.out.printf("%.12f \n",ze);
+
+	}
+
+}
