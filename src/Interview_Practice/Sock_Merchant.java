f5b1e1514a73aa682c03cd2fa156fab2cff3e511
coderbgirl <37058497+coderbgirl@users.noreply.github.com>
2018-10-21 17:15:14 -0400

    Initial

---
+++ src/Interview_Practice/Sock_Merchant.java
@@ -0,0 +1,17 @@
+package Interview_Practice;
+
+import java.util.Scanner;
+
+public class Sock_Merchant {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = scan.nextInt();
+		}
+	}
+
+}
