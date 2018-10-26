a41129bd2248563715190b2b95769902416e24d0
coderbgirl <37058497+coderbgirl@users.noreply.github.com>
2018-10-26 16:37:34 -0400

    interview

---
+++ src/Interview_Practice/MakingAnagrams.java
@@ -0,0 +1,19 @@
+package Interview_Practice;
+
+import java.util.Scanner;
+
+public class MakingAnagrams {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String a = scan.nextLine();
+		String b = scan.nextLine();
+		int result = stringAnagrams(a, b);
+	}
+
+	static int stringAnagrams(String a, String b) {
+		
+		return 1;
+	}
+
+}
